87db987be8b0be7e43ac1b9a16a2280aa1b05d37
Jai Sawhney <jai.sawhney1@gmail.com>
2023-01-23 11:07:46 -0800

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState<string>('')
+
+    const [peperoni, setPeperoni] = useState<boolean>(false)
+    const [pineapple, setPineapple] = useState<boolean>(false)
+    const [anchovies, setAnchovies] = useState<boolean>(false)
+    const [eggplant, setEggplant] = useState<boolean>(false)
+
+    return (
+        <div className='App'>
+            <h1>Pizza Order</h1>
+            <div className={'orderView'}>
+
+                <form className={'toppingInputs'}>
+                    <input placeholder={'Your name'} onChange={e => setName(e.target.value)}/>
+
+                    <label>
+                        <input
+                            type='checkbox'
+                            checked={peperoni}
+                            onChange={() => setPeperoni(!peperoni)}
+                        />
+                        Peperoni
+                    </label>
+
+                    <label>
+                        <input
+                            type='checkbox'
+                            checked={pineapple}
+                            onChange={() => setPineapple(!pineapple)}
+                        />
+                        Pineapple
+                    </label>
+
+                    <label>
+                        <input
+                            type='checkbox'
+                            checked={anchovies}
+                            onChange={() => setAnchovies(!anchovies)}
+                        />
+                        Anchovies
+                    </label>
+
+                    <label>
+                        <input
+                            type='checkbox'
+                            checked={eggplant}
+                            onChange={() => setEggplant(!eggplant)}
+                        />
+                        Eggplant
+                    </label>
+                </form>
+                
+                <div className={'orderDetails'}>
+                    <h2>Your Order</h2>
+                    {name && <p>{name}</p>}
+                    <br/>
+                    {peperoni && <p>Pepperoni</p>}
+                    {pineapple && <p>Pineapple</p>}
+                    {anchovies && <p>Anchovies</p>}
+                    {eggplant && <p>Eggplant</p>}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
